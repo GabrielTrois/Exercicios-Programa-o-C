e1f743930122c072dc1ab5aaa5cafd22eade6217
gabrielalima <gg09065@gmail.com>
2022-10-19 20:13:50 -0300

    correção

--- ex03.c
+++ ex03.c
@@ -2,26 +2,26 @@
 #include <stdlib.h>
 
 int main(){
-    int n = 0, cont[4];
-    cont[1] = 0;
-    cont[2] = 0;
-    cont[3] = 0;
-    cont[4] = 0;
+    int n = 0, cont1 = 0,cont2 = 0,cont3 = 0,
+    cont4 = 0;
 
-    while (n >= 0){
+    printf("digite um numero:\n");
+    scanf("%d", &n);
+    while (n > -1){
+        if (n <= 25)
+            cont1++;
+        else if (n <= 50)
+            cont2++; 
+        else if (n <= 75)
+            cont3++;
+        else if (n <= 100)
+            cont4++;
+        
         printf("digite um numero:\n");
         scanf("%d", &n);
-        if (n <= 25)
-            cont[1]++;
-        else if (n <= 50)
-            cont[2]++; 
-        else if (n <= 75)
-            cont[3]++;
-        else if (n <= 100)
-            cont[4]++;
         
     }
-    printf("%d\n %d\n %d\n %d\n",cont[1],cont[2],cont[3],cont[4]);
+    printf("%d\n%d\n%d\n%d\n",cont1,cont2,cont3,cont4);
 
     system("PAUSE");
     return 0;
